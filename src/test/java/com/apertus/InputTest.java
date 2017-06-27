61eb73d46d9edca964632a29b2d10caa45123d69
Laszlo Levai <levailaszlo86@gmail.com>
2017-06-27 18:22:23 +0200

    all checkable webelements have been added

--- src/test/java/com/apertus/InputTest.java
+++ src/test/java/com/apertus/InputTest.java
@@ -52,26 +52,103 @@ public class InputTest {
         WebElement kompetenciak = driver.findElement(By.xpath("//div[@aria-controls='collapseOne']"));
         WebElement programNev = driver.findElement(By.xpath("//input[@id='filter_programNev']"));
         WebElement programGazda = driver.findElement(By.xpath("//span[@id='select2-filter_programGazdaId-container']"));
+        WebElement programNyilvantartasiSzam = driver.findElement(By.xpath("//input[@id='filter_programNyilvszam']"));
+        WebElement oraszam = driver.findElement(By.xpath("//span[@id='select2-filter_selectedOra-container']"));
+        WebElement tanulmenyiPontszam = driver.findElement(By.xpath("//span[@id='select2-filter_selectedKredit-container']"));
 
 
-        kompetenciak.click();
-        driver.findElement(By.xpath("//div[@id='collapseOne']/div/div[3]/span/input")).click();
-        sleep(5000);
+        //kompetenciak.click();
+        //driver.findElement(By.xpath("//div[@id='collapseOne']/div/div[3]/span/input")).click();
 
         programNev.click();
         programNev.clear();
-        programNev.sendKeys("asdfasdasd");
+        programNev.sendKeys("\"A helyi önkormányzatok törvényességi felügyeletével összefüggő írásbeli kapcsolattartás\" rendszerének gyakorlati bemutatása");
 
         programGazda.click();
         WebElement programGazdaInput = driver.findElement(By.xpath("//input[@role='textbox']"));
         programGazdaInput.click();
         programGazdaInput.clear();
-        programGazdaInput.sendKeys("asdfasdasd");
+        programGazdaInput.sendKeys("NÓGRÁD MEGYEI KORMÁNYHIVATAL");
+
+        programNyilvantartasiSzam.click();
+        programNyilvantartasiSzam.clear();
+        programNyilvantartasiSzam.sendKeys("PM-0733-1404-BS");
+
+        oraszam.click();
+        WebElement oraszamInput = driver.findElement(By.xpath("//input[@autocapitalize='off']"));
+        oraszamInput.click();
+        oraszamInput.clear();
+        oraszamInput.sendKeys("8");
+        oraszamInput.sendKeys(Keys.ENTER);
+
+        tanulmenyiPontszam.click();
+        WebElement tanulmenyiPontszamInput = driver.findElement(By.xpath("//input[@autocapitalize='off']"));
+        tanulmenyiPontszamInput.click();
+        tanulmenyiPontszamInput.clear();
+        tanulmenyiPontszamInput.sendKeys("9");
+        tanulmenyiPontszamInput.sendKeys(Keys.ENTER);
+
+        WebElement koltseg = driver.findElement(By.xpath("//span[@id='select2-filter_selectedKoltseg-container']"));
+        koltseg.click();
+        WebElement koltsegInput = driver.findElement(By.xpath("//input[@autocapitalize='off']"));
+        koltsegInput.click();
+        koltsegInput.clear();
+        koltsegInput.sendKeys("nem");
+        koltsegInput.sendKeys(Keys.ENTER);
+
+        WebElement programCsoport = driver.findElement(By.xpath("//span[@id='select2-filter_selectedPrgcsop-container']"));
+        programCsoport.click();
+        WebElement programCsoportInput = driver.findElement(By.xpath("//input[@autocapitalize='off']"));
+        programCsoportInput.click();
+        programCsoportInput.clear();
+        programCsoportInput.sendKeys("b");
+        programCsoportInput.sendKeys(Keys.ENTER);
+
+        WebElement szervezetreKorlatozott = driver.findElement(By.xpath("//span[@id='select2-filter_selectedSzervezetreKorlatozott-container']"));
+        szervezetreKorlatozott.click();
+        WebElement szervezetreKorlatozottInput = driver.findElement(By.xpath("//input[@autocapitalize='off']"));
+        szervezetreKorlatozottInput.click();
+        szervezetreKorlatozottInput.clear();
+        szervezetreKorlatozottInput.sendKeys("i");
+        szervezetreKorlatozottInput.sendKeys(Keys.ENTER);
+
+
+        WebElement ELearning = driver.findElement(By.xpath("//label[@id='btnSecondaryElearning']"));
+        WebElement jelenleti = driver.findElement(By.xpath("//label[@id='btnSecondaryJelenleti']"));
+        WebElement blended = driver.findElement(By.xpath("//label[@id='btnSecondaryBlended']"));
+        ELearning.click();
+        jelenleti.click();
+        blended.click();
+
+        WebElement telepules = driver.findElement(By.xpath("//span[@id='select2-filter_telepulesId-container']"));
+        telepules.click();
+        WebElement telepulesInput = driver.findElement(By.xpath("//input[@autocapitalize='off']"));
+        telepulesInput.click();
+        telepulesInput.clear();
+        telepulesInput.sendKeys("de");
+        telepulesInput.sendKeys(Keys.ENTER);
+
+
+        WebElement hatalyossagKezdete = driver.findElement(By.xpath("//p-calendar[@id='filter_hatKezd']/span/input"));
+        hatalyossagKezdete.click();
+        hatalyossagKezdete.clear();
+        hatalyossagKezdete.sendKeys("2017.06.27");
+        hatalyossagKezdete.sendKeys(Keys.ENTER);
+
+        WebElement targyevbenIndul = driver.findElement(By.xpath("//p-checkbox[@id='filter_targyevbenIndul']/div/div[2]"));
+        targyevbenIndul.click();
+
+        WebElement kereses = driver.findElement(By.xpath("//button[@type='submit']"));
+        kereses.click();
+
+        sleep(3000);
+        assertTrue("nem jelent meg a program",driver.findElement(By.xpath("//td[contains(.,'PM-0733-1404-BS')]")).isDisplayed());
+        assertTrue("Nincs adat",driver.findElement(By.xpath("//div[@class='alert alert-info']")).isDisplayed());
 
     }
 
     @AfterClass
     public static void tearDownOnce() {
-        driver.close();
+        //driver.close();
     }
 }
---
+++ src/test/java/com/apertus/helper/Kompetenciak.java
@@ -0,0 +1,7 @@
+package com.apertus.helper;
+
+/**
+ * Created by Laco on 2017. 06. 27..
+ */
+public class Kompetenciak {
+}
